9b1f5f8cb05485992f9715b98476dfe902324718
Felix Lange <rayon-zunder.0u@icloud.com>
2024-04-04 21:31:43 +0900

    q align

--- src/components/QuestionBox/questionbox.jsx
+++ src/components/QuestionBox/questionbox.jsx
@@ -8,7 +8,9 @@ function QuestionBox({challenge, correctCount, setCorrectCount}) {
     <div >
       {challenge.map((item, index) => (
         <div className='challenge' key={index}>
-          <p><strong>Q{index + 1}:</strong> {item.question}</p>
+          <div className="question-text">
+            <p><strong>Q{index + 1}:</strong> {item.question}</p>
+          </div>
           <div className='answers'>
             {/* {item.choices ? <Answers choices={item.choices} correctAnswer={item.answer} clicked={clicked} setClicked={setClicked} correctCount={correctCount} setCorrectCount={setCorrectCount} /> : 'Loading...'} */}
             {item.choices ? <Answers choices={item.choices} correctAnswer={item.answer} correctCount={correctCount} setCorrectCount={setCorrectCount} /> : 'Loading...'}
